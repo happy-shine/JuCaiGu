b096798ee66d201545145f1dbe54cb3c1cbd62bd
happy-shine <jojo.song@yahoo.com>
2024-02-07 23:25:43 +0800

    Initial Commit

---
+++ JobUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JobUI
+//
+//  Created by 宋炫熠 on 2024/2/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JobUI/JobUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobUIApp.swift
+//  JobUI
+//
+//  Created by 宋炫熠 on 2024/2/7.
+//
+
+import SwiftUI
+
+@main
+struct JobUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
